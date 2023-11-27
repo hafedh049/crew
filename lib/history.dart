09fcfaf4d8adc68e9188ab0334ad860beae7acb5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 20:06:40 +0100

    Modified  lib/history.dart

--- lib/history.dart
+++ lib/history.dart
@@ -33,7 +33,7 @@ class _HistoryState extends State<History> {
                           children: <Widget>[
                             Container(
                               padding: const EdgeInsets.all(8),
-                              decoration: const BoxDecoration(color: white, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
+                              decoration: const BoxDecoration(boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
                               child: const Text("Content", style: TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w500)),
                             ),
                             const SizedBox(width: 10),
