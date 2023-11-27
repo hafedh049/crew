8679e4317696a5ea7678cf2621eef5d02160f76c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:26:37 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       bottomNavigationBar: GNav(
         tabs: <GButton>[
-          GButton(icon: Font),
+          GButton(icon: Boots),
         ],
       ),
       body: Column(
