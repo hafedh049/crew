315e58eaf653e112cbc7593d9de222fefbd6c056
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 19:55:22 +0100

    Modified  lib/history.dart

--- lib/history.dart
+++ lib/history.dart
@@ -27,7 +27,17 @@ class _HistoryState extends State<History> {
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(_data[index]["date"], style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: const BoxDecoration(color: grey, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
+                              child: const Text("Date", style: TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+                            ),
+                            const SizedBox(width: 10),
+                            Flexible(child: Text(_data[index]["date"], style: const TextStyle(color: pink, fontSize: 14, fontWeight: FontWeight.w600))),
+                          ],
+                        ),
                         const SizedBox(height: 20),
                         Row(
                           children: <Widget>[
