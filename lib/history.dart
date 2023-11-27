20728364f555d2c18dac04cbc8829cdb9508dcfc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 19:56:07 +0100

    Modified  lib/history.dart

--- lib/history.dart
+++ lib/history.dart
@@ -47,7 +47,7 @@ class _HistoryState extends State<History> {
                               child: const Text("Date", style: TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
                             ),
                             const SizedBox(width: 10),
-                            Flexible(child: Text(_data[index]["date"], style: const TextStyle(color: pink, fontSize: 14, fontWeight: FontWeight.w600))),
+                            Flexible(child: Text(Format, style: const TextStyle(color: pink, fontSize: 14, fontWeight: FontWeight.w600))),
                           ],
                         ),
                       ],
