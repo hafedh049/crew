3feb015c323a1516b85188c21da2717892adeeee
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:40:02 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -15,21 +15,21 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
-  rippleColor: Colors.grey.shade700, // tab button ripple color when pressed
-  hoverColor: Colors.grey.shade800, // tab button hover color
-  haptic: true, // haptic feedback
+  rippleColor: Colors.grey.shade700, 
+  hoverColor: Colors.grey.shade800, 
+  haptic: true, 
   tabBorderRadius: 15, 
-  tabActiveBorder: Border.all(color: Colors.black, width: 1), // tab button border
-  tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
-  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
-  curve: Curves.easeOutExpo, // tab animation curves
-  duration:900.ms, // tab animation duration
-  gap: 8, // the tab button gap between icon and text 
-  color: Colors.grey[800], // unselected icon color
-  activeColor: Colors.purple, // selected icon and text color
-  iconSize: 24, // tab button icon size
-  tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
-  padding:const  EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+  tabActiveBorder: Border.all(color: Colors.black, width: 1), 
+  tabBorder: Border.all(color: Colors.grey, width: 1), 
+  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], 
+  curve: Curves.easeOutExpo, 
+  duration:900.ms, 
+  gap: 8, 
+  color: Colors.grey[800], 
+  activeColor: Colors.purple, 
+  iconSize: 24, 
+  tabBackgroundColor: Colors.purple.withOpacity(0.1), 
+  padding:const  EdgeInsets.symmetric(horizontal: 20, vertical: 5), 
   tabs: [
     GButton(
       icon: Bootstrap.qr_code_scan,
