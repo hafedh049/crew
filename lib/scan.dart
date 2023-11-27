2558b45a4156bb9387a2f3387c5cfe01e4dda764
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 18:34:24 +0100

    Modified  lib/scan.dart

--- lib/scan.dart
+++ lib/scan.dart
@@ -52,76 +52,79 @@ class _ScanState extends State<Scan> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: _isOpened
-              ? QRView(key: _qrKey, onQRViewCreated: _onQRViewCreated)
-              : Container(
-                  width: MediaQuery.sizeOf(context).width * .9,
-                  decoration: BoxDecoration(
-                    color: grey.withOpacity(.3),
-                    boxShadow: const <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))],
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: _isOpened
+                ? QRView(key: _qrKey, onQRViewCreated: _onQRViewCreated)
+                : Container(
+                    width: MediaQuery.sizeOf(context).width * .9,
+                    decoration: BoxDecoration(
+                      color: grey.withOpacity(.3),
+                      boxShadow: const <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))],
+                    ),
                   ),
+          ),
+          Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            runAlignment: WrapAlignment.center,
+            runSpacing: 20,
+            spacing: 20,
+            children: <Widget>[
+              InkWell(
+                hoverColor: transparent,
+                highlightColor: transparent,
+                splashColor: transparent,
+                onTap: () async {
+                  setState(() => _isOpened = !_isOpened);
+                },
+                child: AnimatedContainer(
+                  duration: 700.ms,
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
+                  padding: const EdgeInsets.all(8),
+                  child: Text(_isOpened ? "STOP" : "START", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
                 ),
-        ),
-        Wrap(
-          alignment: WrapAlignment.center,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          runAlignment: WrapAlignment.center,
-          runSpacing: 20,
-          spacing: 20,
-          children: <Widget>[
-            InkWell(
-              hoverColor: transparent,
-              highlightColor: transparent,
-              splashColor: transparent,
-              onTap: () async {
-                setState(() => _isOpened = !_isOpened);
-              },
-              child: AnimatedContainer(
-                duration: 700.ms,
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
-                padding: const EdgeInsets.all(8),
-                child: Text(_isOpened ? "STOP" : "START", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
               ),
-            ),
-            InkWell(
-              hoverColor: transparent,
-              highlightColor: transparent,
-              splashColor: transparent,
-              onTap: () async {
-                await _controller!.toggleFlash();
-                setState(() => _isFlash = !_isFlash);
-              },
-              child: AnimatedContainer(
-                duration: 700.ms,
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
-                padding: const EdgeInsets.all(8),
-                child: Text("LIGHT (${_isFlash ? "OFF" : "ON"})", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+              InkWell(
+                hoverColor: transparent,
+                highlightColor: transparent,
+                splashColor: transparent,
+                onTap: () async {
+                  await _controller!.toggleFlash();
+                  setState(() => _isFlash = !_isFlash);
+                },
+                child: AnimatedContainer(
+                  duration: 700.ms,
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
+                  padding: const EdgeInsets.all(8),
+                  child: Text("LIGHT (${_isFlash ? "OFF" : "ON"})", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
               ),
-            ),
-            InkWell(
-              hoverColor: transparent,
-              highlightColor: transparent,
-              splashColor: transparent,
-              onTap: () async {
-                await _controller!.flipCamera();
-                setState(() => _isBack = !_isBack);
-              },
-              child: AnimatedContainer(
-                duration: 700.ms,
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
-                padding: const EdgeInsets.all(8),
-                child: Text("CAMERA (${_isBack ? "REAR" : "FRONT"})", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+              InkWell(
+                hoverColor: transparent,
+                highlightColor: transparent,
+                splashColor: transparent,
+                onTap: () async {
+                  await _controller!.flipCamera();
+                  setState(() => _isBack = !_isBack);
+                },
+                child: AnimatedContainer(
+                  duration: 700.ms,
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: pink),
+                  padding: const EdgeInsets.all(8),
+                  child: Text("CAMERA (${_isBack ? "REAR" : "FRONT"})", style: const TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
