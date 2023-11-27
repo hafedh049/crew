d7aaa85cdac230d1bb9902060782307b56e8b2fd
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:58:42 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -53,7 +53,7 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: PageView.builder(
-        itemBuilder: () {},
+        itemBuilder: (BuildContext context, int index) {},
       ),
     );
   }
