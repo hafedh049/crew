14fc7147ab6163457ae752ab51637eb73a0eaa54
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 19:27:47 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -2,7 +2,7 @@ import 'package:crew/utils/globals.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
-Future<void> load() async {
+Future<Box> load() async {
   Hive.init((await getApplicationDocumentsDirectory()).path);
   box = await Hive.openBox("qr");
 }
