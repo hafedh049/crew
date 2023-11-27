16f1ce830a38f29ff461feed972101156997b9c8
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:39:10 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
   activeColor: Colors.purple, // selected icon and text color
   iconSize: 24, // tab button icon size
   tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
-  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+  padding:const  EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
   tabs: [
     GButton(
       icon: Bootstrap.qr_code_scan,
