19d20e548336eb83a650a34d85659212d1bee15e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 21:18:08 +0100

    Modified  lib/history.dart

--- lib/history.dart
+++ lib/history.dart
@@ -65,7 +65,7 @@ class _HistoryState extends State<History> {
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
                           IconButton(
-                            onPressed: () async => FlutterClipboard.copy('hello flutter friends').then((value) => showSnack("Copied To Clipboard", context)),
+                            onPressed: () async => FlutterClipboard.copy(_data[index]["text"]).then((value) => showSnack("Copied To Clipboard", context)),
                             icon: const Icon(Bootstrap.clipboard2, size: 15, color: pink),
                           ),
                           IconButton(
