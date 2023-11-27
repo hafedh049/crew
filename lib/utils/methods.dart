7a8e3fe0c5f535efbb0943ac03414dc2ef299064
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 19:25:41 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,1 +1,6 @@
-Future<void> load() async {}
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+Future<void> load() async {
+  await Hive.init((await getApplicationDocumentsDirectory()).path)
+}
