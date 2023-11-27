f707e1a7d62137a5b3a596a12305c2a154423612
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 20:05:56 +0100

    Modified  lib/history.dart

--- lib/history.dart
+++ lib/history.dart
@@ -24,7 +24,7 @@ class _HistoryState extends State<History> {
                   child: Container(
                     padding: const EdgeInsets.all(8),
                     margin: const EdgeInsets.all(8),
-                    decoration: const BoxDecoration(color: grey, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
+                    decoration: const BoxDecoration(color: pink, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,11 +33,11 @@ class _HistoryState extends State<History> {
                           children: <Widget>[
                             Container(
                               padding: const EdgeInsets.all(8),
-                              decoration: const BoxDecoration(color: grey, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
-                              child: const Text("Content", style: TextStyle(color: grey, fontSize: 14, fontWeight: FontWeight.w500)),
+                              decoration: const BoxDecoration(color: white, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
+                              child: const Text("Content", style: TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w500)),
                             ),
                             const SizedBox(width: 10),
-                            Flexible(child: Text(_data[index]["text"], style: const TextStyle(color: pink, fontSize: 14, fontWeight: FontWeight.w300))),
+                            Flexible(child: Text(_data[index]["text"], style: const TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w300))),
                           ],
                         ),
                         const SizedBox(height: 20),
