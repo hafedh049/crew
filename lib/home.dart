710f3ba55f8feda69123c79776d519b77f6aeaaf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:37:19 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -14,37 +14,40 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
-        tabs: <GButton>[
-          GButton(
-            icon: Bootstrap.qr_code,
-            text: "Scan",
-            active: true,
-            haptic: true,
-            onPressed: () {},
-          ),
-          GButton(
-            icon: FontAwesome.cubes,
-            text: "Generate",
-            active: true,
-            haptic: true,
-            onPressed: () {},
-          ),
-          GButton(
-            icon: FontAwesome.user_astronaut,
-            text: "Owner",
-            active: true,
-            haptic: true,
-            onPressed: () {},
-          ),
-          GButton(
-            icon: Bootstrap.clock_history,
-            text: "History",
-            active: true,
-            haptic: true,
-            onPressed: () {},
-          ),
-        ],
-      ),
+  rippleColor: Colors.grey[800], // tab button ripple color when pressed
+  hoverColor: Colors.grey[700], // tab button hover color
+  haptic: true, // haptic feedback
+  tabBorderRadius: 15, 
+  tabActiveBorder: Border.all(color: Colors.black, width: 1), // tab button border
+  tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
+  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
+  curve: Curves.easeOutExpo, // tab animation curves
+  duration: Duration(milliseconds: 900), // tab animation duration
+  gap: 8, // the tab button gap between icon and text 
+  color: Colors.grey[800], // unselected icon color
+  activeColor: Colors.purple, // selected icon and text color
+  iconSize: 24, // tab button icon size
+  tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+  tabs: [
+    GButton(
+      icon: LineIcons.home,
+      text: 'Home',
+    ),
+    GButton(
+      icon: LineIcons.heart_o,
+      text: 'Likes',
+    ),
+    GButton(
+      icon: LineIcons.search,
+      text: 'Search',
+    ),
+    GButton(
+      icon: LineIcons.user,
+      text: 'Profile',
+    )
+  ]
+)
       body: const Column(
         children: <Widget>[],
       ),
