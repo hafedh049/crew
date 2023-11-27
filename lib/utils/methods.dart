66c352d117bb3c034e20dd40f015496bf9704f29
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 20:59:31 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -5,3 +5,24 @@ Future<Box> load() async {
   Hive.init((await getApplicationDocumentsDirectory()).path);
   return await Hive.openBox("qr");
 }
+
+void showSnack(String msg){
+                final snackBar = SnackBar(
+                  /// need to set following properties for best effect of awesome_snackbar_content
+                  elevation: 0,
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.transparent,
+                  content: AwesomeSnackbarContent(
+                    title: 'On Snap!',
+                    message:
+                        'This is an example error message that will be shown in the body of snackbar!',
+
+                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                    contentType: ContentType.failure,
+                  ),
+                );
+
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
+              },
