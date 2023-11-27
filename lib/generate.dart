9978dcf016d96be08bec28c25b9fb356502d5aea
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 15:12:44 +0100

    Modified  lib/generate.dart

--- lib/generate.dart
+++ lib/generate.dart
@@ -30,7 +30,7 @@ class _GenerateState extends State<Generate> {
             borderRadius: BorderRadius.circular(5),
             child: Container(
               padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(color: grey, boxShadow: const <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer)]),
+              decoration: BoxDecoration(color: grey, boxShadow: const <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(5, 5))]),
               child: TextField(
                 controller: _inputController,
                 decoration: const InputDecoration(border: InputBorder.none),
