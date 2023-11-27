9e7d31c791d76ea6635474182038626c53bf8ead
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 18:29:28 +0100

    Modified  lib/scan.dart

--- lib/scan.dart
+++ lib/scan.dart
@@ -58,9 +58,9 @@ class _ScanState extends State<Scan> {
           child: _isOpened
               ? QRView(key: _qrKey, onQRViewCreated: _onQRViewCreated)
               : Container(
-                  decoration: const BoxDecoration(
-                    color: grey,
-                    boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))],
+                  decoration: BoxDecoration(
+                    color: grey.withOpacity(.3),
+                    boxShadow: const <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))],
                   ),
                 ),
         ),
