876669d9af67b191f75208bccf7d47b6a611a6d3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 14:53:19 +0100

    Modified  lib/generate.dart

--- lib/generate.dart
+++ lib/generate.dart
@@ -11,6 +11,12 @@ class Generate extends StatefulWidget {
 class _GenerateState extends State<Generate> {
   final QrImage _qrImage = QrImage(QrCode(8, QrErrorCorrectLevel.H));
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
