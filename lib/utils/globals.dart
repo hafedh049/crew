dceb65a21568755fc0b03855de93b6625cbb6d17
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 12:00:34 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -4,4 +4,6 @@ const Color teal = Colors.tealAccent;
 const Color grey = Colors.grey;
 const Color white = Colors.white;
 
-final List<Map<String, dynamic>> screens = <Map<String, dynamic>>[];
+final List<Map<String, dynamic>> screens = <Map<String, dynamic>>[
+  <String, dynamic>{},
+];
