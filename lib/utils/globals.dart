be23f8df874be8a78bf33bc50493df6f91000c6c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 13:45:23 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,7 +5,7 @@ import 'package:crew/scan.dart';
 import 'package:flutter/material.dart';
 
 const Color teal = Colors.tealAccent;
-const Color grey = Colors.grey;
+Color grey = Colors.grey;
 const Color white = Colors.white;
 
 final List<Map<String, dynamic>> screens = <Map<String, dynamic>>[
