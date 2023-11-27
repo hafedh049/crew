8f398c984a7e961b6776ce2986b7024afa3eb7a4
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:00:10 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+    );
   }
 }
