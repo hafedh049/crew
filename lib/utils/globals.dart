132c8b29caf47da903ec1c8c72908d37bcbf91bc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 12:00:24 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color teal = Colors.tealAccent;
 const Color grey = Colors.grey;
 const Color white = Colors.white;
+
+final List<Map<String, dynamic>> screens = <Map<String, dynamic>>[];
