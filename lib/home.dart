61c370fdcdc6e985476fcf9e25b98cab16edc3f6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:48:44 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
         color: grey,
         activeColor: teal,
         iconSize: 25,
-        tabBackgroundColor: Colors.purple.withOpacity(0.1),
+        tabBackgroundColor: teal,
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tabs: <GButton>[
           GButton(
