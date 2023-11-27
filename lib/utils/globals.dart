c9a023785ec437841f95cf7be8e64874e642e45e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 21:33:15 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -9,12 +9,7 @@ const Color grey = Color.fromARGB(255, 51, 56, 66);
 const Color white = Colors.white;
 const Color transparent = Colors.transparent;
 
-const List<Widget> screens = <Widget>[
-  Scan(),
-  Generate(),
-  Owner(),
-  History(),
-];
+const List<Widget> screens = <Widget>[Scan(), Generate(), History()];
 
 int currentIndex = 0;
 Box? box;
