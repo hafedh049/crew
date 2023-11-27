4a25c3f26e1b6527f9da92621d6589ece3aa519c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 11:41:12 +0100

    ModifiedM lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -15,41 +15,44 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
-  rippleColor: Colors.grey.shade700, 
-  hoverColor: Colors.grey.shade800, 
-  haptic: true, 
-  tabBorderRadius: 15, 
-  tabActiveBorder: Border.all(color: Colors.black, width: 1), 
-  tabBorder: Border.all(color: Colors.grey, width: 1), 
-  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], 
-  curve: Curves.easeOutExpo, 
-  duration:900.ms, 
-  gap: 8, 
-  color: Colors.grey[800], 
-  activeColor: Colors.purple, 
-  iconSize: 24, 
-  tabBackgroundColor: Colors.purple.withOpacity(0.1), 
-  padding:const  EdgeInsets.symmetric(horizontal: 20, vertical: 5), 
-  tabs: [
-    GButton(
-      icon: Bootstrap.qr_code_scan,
-      text: 'Home',
-      onPressed: (){},
-    ),
-    GButton(
-      icon: FontAwesome.cubes,
-      text: 'Likes',onPressed: (){},
-    ),
-    GButton(
-      icon: FontAwesome.user,
-      text: 'Search',   onPressed: (){},
-    ),
-    GButton(
-      icon: LineIcons.user,
-      text: 'Profile',   onPressed: (){},
-    )
-  ]
-)
+        rippleColor: Colors.grey.shade700,
+        hoverColor: Colors.grey.shade800,
+        haptic: true,
+        tabBorderRadius: 15,
+        tabActiveBorder: Border.all(color: Colors.black, width: 1),
+        tabBorder: Border.all(color: Colors.grey, width: 1),
+        tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)],
+        curve: Curves.easeOutExpo,
+        duration: 900.ms,
+        gap: 8,
+        color: Colors.grey[800],
+        activeColor: Colors.purple,
+        iconSize: 24,
+        tabBackgroundColor: Colors.purple.withOpacity(0.1),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tabs: [
+          GButton(
+            icon: Bootstrap.qr_code_scan,
+            text: 'Home',
+            onPressed: () {},
+          ),
+          GButton(
+            icon: FontAwesome.cubes,
+            text: 'Likes',
+            onPressed: () {},
+          ),
+          GButton(
+            icon: FontAwesome.user,
+            text: 'Search',
+            onPressed: () {},
+          ),
+          GButton(
+            icon: LineIcons.user,
+            text: 'Profile',
+            onPressed: () {},
+          )
+        ],
+      ),
       body: const Column(
         children: <Widget>[],
       ),
