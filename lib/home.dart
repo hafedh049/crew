a693c3c6175f1822e8c3cbc02d4d8ac0774977ab
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 19:29:41 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -100,7 +100,7 @@ class _HomeState extends State<Home> {
                   itemBuilder: (BuildContext context, int index) => screens[index]["screen"],
                 );
               } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator(color: pink);
+                return const Center(child:  CircularProgressIndicator(color: pink));
               } else {return}
             },
           ),
