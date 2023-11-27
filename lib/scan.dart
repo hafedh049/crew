bf9167bb309fae4df13b344ec20aa64af1884326
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-27 20:56:01 +0100

    Modified  lib/scan.dart

--- lib/scan.dart
+++ lib/scan.dart
@@ -57,6 +57,7 @@ class _ScanState extends State<Scan> {
               decoration: const BoxDecoration(color: grey, boxShadow: <BoxShadow>[BoxShadow(color: pink, blurStyle: BlurStyle.outer, offset: Offset(2, 2))]),
               child: Row(
                 children: <Widget>[
+                  IconButton(onPressed: () => _inputController.clear(), icon: const Icon(Bootstrap.x_diamond, size: 15, color: pink)),
                   const SizedBox(width: 20),
                   Text(scanData.code!, style: const TextStyle(color: white, fontSize: 14, fontWeight: FontWeight.w500)),
                 ],
